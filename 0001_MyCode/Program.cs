bcce7f400db375ca28a40c2293b393eb2f47409e
redo_oreon <vladsok1994@gmail.com>
2022-08-04 23:32:25 +0300

    added StringWrapper

--- 0001_MyCode/Program.cs
+++ 0001_MyCode/Program.cs
@@ -7,4 +7,21 @@ namespace _0001_MyCode
     {
         static void Main( string[] args ) { }
     }
+
+    class StringWrapper
+    {
+        string _str;
+        public StringWrapper( string str ) { _str = str; }
+
+        public static string operator *( StringWrapper wrapper, int count )
+        {
+            var sb = new StringBuilder( );
+            for (int i = 0; i < count; i++)
+            {
+                sb.Append(wrapper._str);
+            }
+
+            return sb.ToString( );
+        }
+    }
 }
